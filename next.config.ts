808f62399f4f29152b52e99f6b06a180a915e3f1
Vijay Misal <misalvijay153@gmail.com>
2025-02-12 22:12:53 +0530

    things updating

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
     // swcMinify: false // it should be false by default 
+    eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
